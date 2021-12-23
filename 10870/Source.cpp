4c7d7d1a9bd2abf7c4b40df35086f3db93b8da4e
AlmondGood <88735581+almondgood@users.noreply.github.com>
2021-12-23 20:43:30 +0900

    Update Source.cpp

--- 10870/Source.cpp
+++ 10870/Source.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
 
-int pibonaci(int cnt, int n);
-
-int main() {
-	
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
+int fibo(int ans, int n);
+int main()
+{
 	int n = 0;
-
 	cin >> n;
-
-	cout << pibonaci(0, n);
-
+	cout << fibo(0, n);
 	return 0;
 }
 
-int pibonaci(int cnt, int n)
-{
-	int pb = 0;
-	if (n == 0) { return 0; }
-	else if (n == 1) { return 1; }
-	else if (cnt == n) {
-		return pb;
-	}
-	else { pb += pibonaci(cnt, n) + pibonaci(cnt + 1, n); }
-
-	return pb;
+int fibo(int ans, int n){
+    
+    if(n < 2) {return n;}
+    else {
+        ans += fibo(ans, n - 1) + fibo(ans, n - 2);
+    }
+    return ans;
 }
