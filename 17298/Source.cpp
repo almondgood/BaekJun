aabf425f24905e49f23c260053c9fe28e8a88847
AlmondGood <jwt2358@naver.com>
2021-12-29 12:46:21 +0900

    골드 4

---
+++ 17298/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int arr1[1000001];
+int arr2[1000001];
+stack<int> s;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr1[i];
+	}
+	
+	for (int i = n - 1; i >= 0; i--) {
+	
+		while (!s.empty() && arr1[i] >= s.top()) {
+			s.pop();
+		}
+
+		if (!s.empty() && arr1[i] < s.top()) {
+			arr2[i] = s.top();
+		}
+		else arr2[i] = -1;
+
+		s.push(arr1[i]);
+	}
+	
+	for (int i = 0; i < n; i++) {
+		cout << arr2[i] << ' ';
+	}
+
+	return 0;
+}
