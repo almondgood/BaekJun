802189d6c1dd7e7ecbb129c2602b86c2560a4df6
AlmondGood <jwt2358@naver.com>
2022-01-23 23:42:50 +0900

    브론즈 2

---
+++ 17608/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int n, count = 0;
+	cin >> n;
+	stack<int> s;
+
+	for (int i = 1; i <= n; i++) {
+		int tmp;
+		cin >> tmp;
+		s.push(tmp);
+	}
+
+	int tmp = 0;
+	for (int i = 0; i < n; i++) {
+		if (s.top() > tmp) {
+			count++;
+			tmp = s.top();
+		}
+		s.pop();
+	}
+	cout << count;
+
+	return 0;
+}
